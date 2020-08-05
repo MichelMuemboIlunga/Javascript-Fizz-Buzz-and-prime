93fc1d07a0cc846b1be67aa44b4991f7af693ff1
MichelMuemboIlunga <61247320+MichelMuemboIlunga@users.noreply.github.com>
2020-08-06 00:04:54 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+for (var n = 1; n < 101; n++) {
+  if (n % 3 === 0 && n % 5 === 0) {
+    console.log(n + " FizzBuzz");
+  } else if (n % 3 === 0) {
+    console.log(n + " Fizz");
+  } else if (n % 5 === 0) {
+    console.log(n + " Buzz");
+  } else if (isPrime(n) === true) {
+    console.log(n + " prime");
+  } else {
+    console.log(n);
+  }
+}
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  let prime = true;
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return prime;
+}
